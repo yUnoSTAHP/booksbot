59a2f47c307c9c70141abff7413441ef8ec8991b
yUnoSTAHP <dominicfeer@hotmail.com>
2020-08-08 13:38:15 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -10,21 +10,10 @@ class BooksSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+            yield {
+                'email': quote.xpath('//a[@data-analytics='link.contact.email']').get()
+            }
 
-    def parse_book_page(self, response):
-        item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
-        yield item
+        next_page = response.css('ul.paginator li.pageend a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
